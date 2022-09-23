da83993f1ec081e61db32dfc17de2201c7b367d9
ilis137 <iliyasshahapure1@gmail.com>
2022-09-23 15:16:27 +0530

    welcome message

---
+++ src/main/java/com/javapractice/App.java
@@ -0,0 +1,15 @@
+package com.javapractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App 
+{
+    private static final Logger log = LogManager.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Generics program!" );
+    }
+}
+
